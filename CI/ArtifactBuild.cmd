5bc3b80875c663698a2ad9299f448aab22c00508
Noxalus <noxalus@gmail.com>
2021-06-09 13:44:28 +0200

    Try to upload artifact in the GitHub action.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -3,8 +3,8 @@
 pushd "%~dp0"
 cd ..\KHFM-VF-Patch
 
-powershell Compress-7Zip "bin\x86\Release" -ArchiveFileName "..\SampleX86.zip" -Format Zip
-powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "..\SampleX64.zip" -Format Zip
+@REM powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "..\KHFM-VF-Patch-x64.zip" -Format Zip
+powershell Compress-7Zip "bin\x86\Release" -ArchiveFileName "..\KHFM-VF-Patch-x86.zip" -Format Zip
 
 :exit
 popd
--- CI/Build.cmd
+++ CI/Build.cmd
@@ -7,7 +7,7 @@ cd ..\KHFM-VF-Patch
 @REM if exist Release rd /s /q Release
 @REM if exist x64 rd /s /q x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Restore /p:Configuration=Release /p:Platform=x64
+@REM "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Restore /p:Configuration=Release /p:Platform=x64
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Restore /p:Configuration=Release /p:Platform=x86
 
 :exit
