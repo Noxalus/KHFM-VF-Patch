bd816596ffd041f9a724bddf7bd1904fec7efb96
Noxalus <noxalus@gmail.com>
2021-06-09 13:07:02 +0200

    Try to fix CI.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -1,11 +1,10 @@
 @echo off
 
 pushd "%~dp0"
+cd ..\KHFM-VF-Patc
 
-cd ..\KHFM-VF-Patch\bin
-
-powershell Compress-7Zip "x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
+powershell Compress-7Zip "bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip
+powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
 
 :exit
 popd
--- CI/Build.cmd
+++ CI/Build.cmd
@@ -7,8 +7,8 @@ cd ..\KHFM-VF-Patch
 @REM if exist Release rd /s /q Release
 @REM if exist x64 rd /s /q x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x64
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Restore /p:Configuration=Release /p:Platform=x64
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Restore /p:Configuration=Release /p:Platform=x86
 
 :exit
 popd
