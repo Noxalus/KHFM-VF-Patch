1d2a86d7ccd34501899468b946bc739d1b009613
Noxalus <noxalus@gmail.com>
2021-06-09 13:10:01 +0200

    Try to fix CI.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 pushd "%~dp0"
-cd ..\KHFM-VF-Patc
+cd ..\KHFM-VF-Patch
 
 powershell Compress-7Zip "bin\x86\Release" -ArchiveFileName "SampleX86.zip" -Format Zip
 powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
