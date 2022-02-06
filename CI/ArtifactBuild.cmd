1b8cbd367dc30cfbc92198813fe297ce092312ed
Noxalus <noxalus@gmail.com>
2022-02-06 15:25:59 +0100

    Move CI sub-patches compression from Build to Artifact stage.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -3,10 +3,19 @@
 pushd "%~dp0"
 cd ..\KHFM-VF-Patch
 
+powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-EN" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-EN.zip" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-FR" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-FR.zip" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Stranger" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Stranger.zip" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Textures" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Textures.zip" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Voices" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Voices.zip" -Format Zip
+
+@REM Images should be copied during the build, but for an unknown reason it doesn't work as expected
+powershell Copy-Item "Resources\Images" -Destination "bin\x86\Release\net5.0-windows\Resources"
+
 @REM powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "..\KHFM-VF-Patch-x64.zip" -Format Zip
 powershell Compress-7Zip "bin\x86\Release\net5.0-windows" -ArchiveFileName "KHFM-VF-Patch-x86.zip" -Format Zip
 
-echo %VERSION%
+echo "Version: %VERSION%"
 
 dir
 
--- CI/Build.cmd
+++ CI/Build.cmd
@@ -12,21 +12,7 @@ cd ..\KHFM-VF-Patch
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Build /p:Configuration=Release /p:Platform=x86
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" KHFM-VF-Patch.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86
 
-powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-EN" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-EN.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-FR" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-FR.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Stranger" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Stranger.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Textures" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Textures.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Voices" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Voices.zip" -Format Zip
-
-dir
-
 :exit
 popd
-
-dir
-
-cd ..
-
-dir
 
 @REM @echo on
