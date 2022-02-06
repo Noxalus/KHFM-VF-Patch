591291b2e32411ccb759c66e673f4a6aaa5b30bd
Noxalus <noxalus@gmail.com>
2022-02-06 16:08:01 +0100

    Make sure to move sub-patches to the right place for the CI artifact.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -3,11 +3,13 @@
 pushd "%~dp0"
 cd ..\KHFM-VF-Patch
 
-powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-EN" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-EN.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-FR" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Magic-FR.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Stranger" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Stranger.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Textures" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Textures.zip" -Format Zip
-powershell Compress-7Zip "Resources\Patches\KH1FM-Voices" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Voices.zip" -Format Zip
+powershell New-Item -Path "bin\x86\Release\net5.0-windows\Resources\Patches" -ItemType Directory
+
+powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-EN" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\Patches\KH1FM-Magic-EN.patch" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Magic-FR" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\Patches\KH1FM-Magic-FR.patch" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Stranger" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\Patches\KH1FM-Stranger.patch" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Textures" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\Patches\KH1FM-Textures.patch" -Format Zip
+powershell Compress-7Zip "Resources\Patches\KH1FM-Voices" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\Patches\KH1FM-Voices.patch" -Format Zip
 
 echo "Version: %VERSION%"
 
