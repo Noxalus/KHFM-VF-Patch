03c4ef25d868d33f599def4e0451aa2584afabbe
Noxalus <noxalus@gmail.com>
2022-02-06 15:57:02 +0100

    Only zip sub-patches in the ArtifactBuild script for the CI.

--- CI/ArtifactBuild.cmd
+++ CI/ArtifactBuild.cmd
@@ -9,19 +9,7 @@ powershell Compress-7Zip "Resources\Patches\KH1FM-Stranger" -ArchiveFileName "bi
 powershell Compress-7Zip "Resources\Patches\KH1FM-Textures" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Textures.zip" -Format Zip
 powershell Compress-7Zip "Resources\Patches\KH1FM-Voices" -ArchiveFileName "bin\x86\Release\net5.0-windows\Resources\KH1FM-Voices.zip" -Format Zip
 
-@REM Images should be copied during the build, but for an unknown reason it doesn't work as expected
-powershell Copy-Item "Resources\Images" -Destination "bin\x86\Release\net5.0-windows\Resources"
-
-@REM powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "..\KHFM-VF-Patch-x64.zip" -Format Zip
-powershell Compress-7Zip "bin\x86\Release\net5.0-windows" -ArchiveFileName "KHFM-VF-Patch-x86.zip" -Format Zip
-
 echo "Version: %VERSION%"
-
-dir
-
-cd "bin\x86\Release\net5.0-windows\Resources"
-
-dir
 
 :exit
 popd
