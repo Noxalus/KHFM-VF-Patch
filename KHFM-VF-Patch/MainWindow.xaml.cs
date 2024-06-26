84b86da3adf5d5fa394b6449ca452df23c8f9213
Noxalus <noxalus@gmail.com>
2024-06-26 18:10:11 +0200

    Remove useless using.

--- KHFM-VF-Patch/MainWindow.xaml.cs
+++ KHFM-VF-Patch/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
